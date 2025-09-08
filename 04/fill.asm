7592b82d70d3238c2a827919e4a3fe2a0f2e5c8d
MohammadLashkari <moe.lashkari@gmail.com>
2025-09-08 22:24:35 +0330

    refactor mult

---
+++ 04/fill.asm
@@ -0,0 +1,3 @@
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed the program blackens the screen,
+// When no key is pressed, the screen should be cleared.
--- 04/mult.asm
+++ 04/mult.asm
@@ -4,33 +4,7 @@
     @R2
     M=0
 
-    @R0
-    D=M
-
-    // if R0 > R1 goto SWAP
-    @R1
-    D=D-M
-    @SWAP
-    D;JGT
-
-(SWAP)
-    @R0
-    D=M
-    @temp
-    M=D
-
-    @R1
-    D=M
-    @R0
-    M=D
-    
-    @temp
-    D=M
-    @R1
-    M=D
-
 (LOOP)
-
     @R0
     D=M
     @END
@@ -40,7 +14,6 @@
     D=M
     @R2
     M=D+M
-
 
     @R0
     M=M-1
