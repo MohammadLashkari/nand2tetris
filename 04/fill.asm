6521afd92f055f1eac96b4aedb989416e8c93040
MohammadLashkari <moe.lashkari@gmail.com>
2025-09-10 15:30:29 +0330

    implement fill

--- 04/fill.asm
+++ 04/fill.asm
@@ -1,3 +1,57 @@
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed the program blackens the screen,
 // When no key is pressed, the screen should be cleared.
+
+
+(LOOP)
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+    @WHITE
+    0;JMP
+
+
+    @LOOP
+    0;JMP
+
+(BLACK)
+    @c
+    M=-1
+    @COLOR
+    0;JMP
+
+
+(WHITE)
+    @c
+    M=0
+    @COLOR
+    0;JMP
+
+
+(COLOR)
+    @KBD
+    D=A
+    @i
+    M=D-1
+
+(LOOP2)
+    // if screen colored goto LOOP
+    @i
+    D=M
+    @SCREEN
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    @c
+    D=M
+    @i
+    A=M
+    M=D
+    @i
+    M=M-1
+    @LOOP2
+    0;JMP
+
+
